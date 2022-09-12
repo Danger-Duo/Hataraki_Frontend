17529fbb344e8b6209da15a821e7ad78eb027b4a
Darren Sim <45655404+Kalsyc@users.noreply.github.com>
2022-09-13 00:30:06 +0800

    Add in Noto Sans font

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,7 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import HomeHeader from '../components/header/HomeHeader.vue';
+</script>
 
 <template>
-  <main></main>
+  <HomeHeader />
 </template>
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,16 @@
 module.exports = {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
+    colors: {
+      'primary-font': '#00204a',
+      'light-bg': '#d9faff',
+      'secondary-font': '#00367d',
+      'dark-bg': '#000a17',
+      'dark-font': '#f8f8f8',
+    },
+    fontFamily: {
+      sans: ['Noto Sans', 'sans-serif'],
+    },
     extend: {},
   },
   plugins: [],
