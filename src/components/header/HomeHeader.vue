3f2282ea704b909642180214879cf3a9b0f3cc62
Darren Sim <45655404+Kalsyc@users.noreply.github.com>
2022-09-16 16:41:39 +0800

    Redo icons and logo

--- src/components/header/HomeHeader.vue
+++ src/components/header/HomeHeader.vue
@@ -1,48 +1,55 @@
 <script setup lang="ts">
-  import { ref, type Ref } from 'vue';
-  import HomeToLoginButton from '../button/HomeToLoginButton.vue';
+import router from '@/router';
+import { ref, type Ref } from 'vue';
+import HomeToLoginButton from '../button/HomeToLoginButton.vue';
+import HatarakiSVGLogo from '../svg-icons/hataraki/HatarakiSVGLogo.vue';
+import HatarakiSVGLogoDark from '../svg-icons/hataraki/HatarakiSVGLogoDark.vue';
 
-  function toggleDarkMode(): void {
-    const htmlElement: HTMLHtmlElement = document.getElementsByTagName('html')[0];
-    isDarkModeState.value = !isDarkModeState.value;
-    if (isDarkModeState.value) {
-      localStorage.setItem('isDarkMode', 'true');
-      htmlElement.classList.add('dark');
-    } else {
-      localStorage.setItem('isDarkMode', 'false');
-      htmlElement.classList.remove('dark');
-    }
-  }
+const isDarkMode: boolean = localStorage.getItem('isDarkMode') == 'true';
+if (isDarkMode) {
+  const htmlElement: HTMLHtmlElement = document.getElementsByTagName('html')[0];
+  htmlElement.classList.add('dark');
+}
+const isDarkModeState: Ref<boolean> = ref<boolean>(isDarkMode);
 
-  const isDarkMode: boolean = localStorage.getItem('isDarkMode') == 'true';
-  if (isDarkMode) {
-    const htmlElement: HTMLHtmlElement = document.getElementsByTagName('html')[0];
+function toggleDarkMode(): void {
+  const htmlElement: HTMLHtmlElement = document.getElementsByTagName('html')[0];
+  isDarkModeState.value = !isDarkModeState.value;
+  if (isDarkModeState.value) {
+    localStorage.setItem('isDarkMode', 'true');
     htmlElement.classList.add('dark');
+  } else {
+    localStorage.setItem('isDarkMode', 'false');
+    htmlElement.classList.remove('dark');
   }
-  const isDarkModeState: Ref<boolean> = ref<boolean>(isDarkMode);
+}
+
+function routeHome(): void {
+  router.push('/');
+}
 </script>
 
 <template>
   <div class="w-full h-20 flex flex-row justify-between">
-    <img v-if="!isDarkModeState" src="../../assets/hataraki-logo-transparent.png" class="h-full" alt="" />
-    <img v-else src="../../assets/hataraki-logo-dark.png" class="h-full" alt="" />
-    <div class="h-full flex flex-row gap-5 items-center mx-8">
+    <HatarakiSVGLogo v-if="!isDarkModeState" class="h-5/6 cursor-pointer w-auto" @click="routeHome" />
+    <HatarakiSVGLogoDark v-else class="h-5/6 cursor-pointer w-auto" @click="routeHome" />
+    <div class="h-full flex flex-row gap-4 items-center mx-8">
       <img
         v-if="!isDarkModeState"
-        src="../../assets/icons/light-mode-icon.png"
+        src="../../assets/icons/light-mode.png"
         alt=""
-        class="h-2/6 hover:cursor-pointer hover:opacity-50"
+        class="h-2/6 hover:cursor-pointer hover:opacity-50 transition-all ease-out duration-700"
         @click="toggleDarkMode"
       />
       <img
         v-else
-        src="../../assets/icons/dark-mode-icon.png"
+        src="../../assets/icons/dark-mode.png"
         alt=""
-        class="h-2/6 hover:cursor-pointer hover:opacity-50 dark:invert"
+        class="h-2/6 hover:cursor-pointer hover:opacity-50 dark:invert transition-all ease-out duration-700"
         @click="toggleDarkMode"
       />
-      <a class="h-3/6 hover:opacity-50" href="https://github.com/Danger-Duo/Hataraki_Frontend">
-        <img src="../../assets/github-mark.png" alt="" class="h-full dark:invert" />
+      <a class="h-3/6 hover:opacity-50 transition-all ease-out duration-700" href="https://github.com/Danger-Duo/Hataraki_Frontend">
+        <img src="../../assets/icons/github-mark.png" alt="" class="h-full dark:invert" />
       </a>
       <HomeToLoginButton :link="'login'" :text="'Login'" />
     </div>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,5 +5,10 @@
 <template>
   <div class="bg-light h-screen dark:bg-dark">
     <HomeHeader />
+    <div class="w-full py-40">
+      <div>
+        <span>The next-gen ATS</span>
+      </div>
+    </div>
   </div>
 </template>
