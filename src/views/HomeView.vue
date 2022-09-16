5d813e62c017c9bdec56b0037aee6499a2614a91
Darren Sim <45655404+Kalsyc@users.noreply.github.com>
2022-09-16 19:49:41 +0800

    Fix padding on home

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -13,10 +13,10 @@ import HomeToLoginButton from '../components/button/HomeToLoginButton.vue';
       <HomeToLoginButton class="w-fit my-2" :link="'login'" :text="'Get Started Here'" />
     </div>
 
-    <div class="w-full p-8 lg:py-8 lg:px-[22em] flex flex-col">
+    <div class="w-full p-8 lg:py-8 lg:px-[16%] flex flex-col">
       <span class="my-6 text-5xl text-center text-primary dark:text-dark font-bold">Why use Hataraki?</span>
 
-      <div class="lg:w-[60%] my-6 flex flex-col">
+      <div class="lg:w-[50%] my-6 flex flex-col">
         <span class="my-2 lg:text-3xl text-xl text-secondary dark:text-dark font-bold">Create your own hiring workflow</span>
         <span class="my-2 text-xl text-secondary dark:text-dark font-normal"
           >Tired of sending emails for every application on LinkedIn? Craft a template or choose from our many carefully-curated templates and have
@@ -25,7 +25,7 @@ import HomeToLoginButton from '../components/button/HomeToLoginButton.vue';
         >
       </div>
 
-      <div class="lg:w-[60%] my-6 flex flex-col">
+      <div class="lg:w-[50%] my-6 flex flex-col">
         <span class="my-2 lg:text-3xl text-xl text-secondary dark:text-dark font-bold">Conduct any technical assessment with ease</span>
         <span class="my-2 text-xl text-secondary dark:text-dark font-normal"
           >Want your panel of interviewers to be updated on the latest candidates? No worries! Hataraki provides the latest information on your
@@ -33,7 +33,7 @@ import HomeToLoginButton from '../components/button/HomeToLoginButton.vue';
         </span>
       </div>
 
-      <div class="lg:w-[60%] my-6 flex flex-col">
+      <div class="lg:w-[50%] my-6 flex flex-col">
         <span class="my-2 lg:text-3xl text-xl text-secondary dark:text-dark font-bold">Closing off correspondence with ease</span>
         <span class="my-2 text-xl text-secondary dark:text-dark font-normal"
           >Want your candidates to be informed of the current application status? Hataraki allows you to send a scheduled email to remind them that
@@ -41,7 +41,7 @@ import HomeToLoginButton from '../components/button/HomeToLoginButton.vue';
         </span>
       </div>
 
-      <div class="lg:w-[60%] my-6 flex flex-col">
+      <div class="lg:w-[50%] my-6 flex flex-col">
         <span class="my-2 lg:text-3xl text-xl text-secondary dark:text-dark font-bold">No interruption to your current workflow</span>
         <span class="my-2 text-xl text-secondary dark:text-dark font-normal"
           >Not used to hataraki? Fret not! You may choose to integrate and sync hataraki to your email inbox or use any of our available plugins and
@@ -49,7 +49,7 @@ import HomeToLoginButton from '../components/button/HomeToLoginButton.vue';
         </span>
       </div>
 
-      <div class="lg:w-[60%] my-6 flex flex-col">
+      <div class="lg:w-[50%] my-6 flex flex-col">
         <span class="my-2 lg:text-3xl text-xl text-secondary dark:text-dark font-bold">No subscription, you pay what you need</span>
         <span class="my-2 text-xl text-secondary dark:text-dark font-normal"
           >You pay what you need and for the features that you want. In fact, hataraki was designed to be free and usable for small teams, startups
