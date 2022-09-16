cfe4a70c9e7f491d39c6a8733733b9babc762c97
Darren Sim <45655404+Kalsyc@users.noreply.github.com>
2022-09-16 22:45:24 +0800

    Shift background to layout wrapping RouterView

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts">
-  import { RouterView } from 'vue-router';
+import { RouterView } from 'vue-router';
 </script>
 
 <template>
-  <RouterView />
+  <div class="bg-light min-h-screen dark:bg-dark">
+    <RouterView />
+  </div>
 </template>
