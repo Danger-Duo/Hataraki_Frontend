8ce20beb81f0ab510d74927a2f039721d513e5dd
Darren Sim <45655404+Kalsyc@users.noreply.github.com>
2022-09-17 14:18:40 +0800

    Do up custom input elements

---
+++ src/components/input/TextInput.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+const props = defineProps({
+  modelValue: { type: String, required: true },
+  labelFor: { type: String, required: true },
+});
+defineEmits(['update:modelValue']);
+</script>
+
+<template>
+  <div class="flex flex-col">
+    <label :for="props.labelFor" class="m-2 text-primary dark:text-dark font-bold"><slot></slot></label>
+    <input
+      :value="props.modelValue"
+      @input="$emit('update:modelValue', ($event.target as HTMLInputElement).value)"
+      class="bg-input text-primary rounded-lg border-none m-2 px-4 py-2"
+      spellcheck="false"
+      type="text"
+    />
+  </div>
+</template>
--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -3,16 +3,28 @@ import HatarakiSVGLogo from '../components/svg-icons/hataraki/HatarakiSVGLogo.vu
 import HatarakiSVGLogoDark from '../components/svg-icons/hataraki/HatarakiSVGLogoDark.vue';
 import ActionButton from '../components/button/ActionButton.vue';
 import { type Ref, ref } from 'vue';
+import TextInput from '../components/input/TextInput.vue';
+import PasswordInput from '../components/input/PasswordInput.vue';
 
 const isDarkMode: boolean = localStorage.getItem('isDarkMode') == 'true';
 const isLogin: Ref<boolean> = ref<boolean>(true);
 
+const loginEmail: Ref<string> = ref<string>('');
+const loginPassword: Ref<string> = ref<string>('');
+
+const registerEmail: Ref<string> = ref<string>('');
+const registerPassword: Ref<string> = ref<string>('');
+const registerConfirmPassword: Ref<string> = ref<string>('');
+
 function loginUser(): void {
-  console.log('Log in user');
+  console.log('Login Email: ', loginEmail.value);
+  console.log('Login Password: ', loginPassword.value);
 }
 
 function registerUser(): void {
-  console.log('Register User');
+  console.log('Register Email: ', registerEmail.value);
+  console.log('Register Password: ', registerPassword.value);
+  console.log('Register Confirm Password: ', registerConfirmPassword.value);
 }
 
 function toggleLoginRegister(): void {
@@ -21,23 +33,30 @@ function toggleLoginRegister(): void {
 </script>
 
 <template>
-  <div class="w-full min-h-screen flex items-center justify-center animate__animated animate__slideInDown">
+  <div class="w-full min-h-screen flex items-center justify-center">
     <div v-if="isLogin" class="flex flex-col justify-center">
       <HatarakiSVGLogo v-if="!isDarkMode" class="h-auto w-[300px] m-5" />
       <HatarakiSVGLogoDark v-else class="h-auto w-[300px] m-5" />
-      <div class="flex flex-col">
-        <label for="email" class="m-2 text-primary dark:text-dark font-bold">Email Address</label>
-        <input class="bg-input text-primary rounded-lg border-none m-2 px-4 py-2" spellcheck="false" type="text" />
-      </div>
-      <div class="flex flex-col">
-        <label for="email" class="m-2 text-primary dark:text-dark font-bold">Password</label>
-        <input class="bg-input text-primary rounded-lg border-none m-2 px-4 py-2" spellcheck="false" type="password" />
-      </div>
+      <TextInput v-model="loginEmail" label-for="loginEmail">Email Address</TextInput>
+      <PasswordInput v-model="loginPassword" label-for="loginPassword">Password</PasswordInput>
       <div class="my-4 w-full">
         <ActionButton :action="loginUser">Log In</ActionButton>
       </div>
       <div class="my-4 w-full">
         <ActionButton :action="toggleLoginRegister">Create New Account</ActionButton>
+      </div>
+    </div>
+    <div v-else class="flex flex-col justify-center">
+      <HatarakiSVGLogo v-if="!isDarkMode" class="h-auto w-[300px] m-5" />
+      <HatarakiSVGLogoDark v-else class="h-auto w-[300px] m-5" />
+      <TextInput v-model="registerEmail" label-for="loginEmail">Email Address</TextInput>
+      <PasswordInput v-model="registerPassword" label-for="loginPassword">Password</PasswordInput>
+      <PasswordInput v-model="registerConfirmPassword" label-for="loginPassword">Confirm Password</PasswordInput>
+      <div class="my-4 w-full">
+        <ActionButton :action="registerUser">Register</ActionButton>
+      </div>
+      <div class="my-4 w-full">
+        <ActionButton :action="toggleLoginRegister">Have an account? Log in here!</ActionButton>
       </div>
     </div>
   </div>
